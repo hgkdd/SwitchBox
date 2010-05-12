f8fabb55d2bb4aa9c72f5b199d67dfdd50d56121
hgkdd <devnull@localhost>
2010-05-12 14:12:54 +0200

    tested

--- RCSwitch_main.py
+++ RCSwitch_main.py
@@ -11,38 +11,90 @@ class SWController(QtGui.QMainWindow):
         self.sw=sw
         print sw
         #save settings
-        self.sw.ask('R1P0R2P0R3P1R4P1R5P0R6P0'*2)
+        self.ask('R1P0R2P0R3P1R4P1R5P0R6P0')
         self.RxLF=True
         self.RxAtt=True
         self.RxPM=True
         QtGui.QMainWindow.__init__(self)
         self.ui=ui()
         self.ui.setupUi(self)
-        
+    
+    def ask(self, cmd):
+        ans=self.sw.ask(cmd)
+        print "CMD: %s\nANS: %s"%(cmd, ans)
+        rp=[int(ans[4*i+3:4*i+4]) for i in range(6)]
+        rp.insert(0, None) # rp[1]=relais_position of R1 etc
+        print rp[1:]
+        # K1, K2, K3
+        K1='C-%d'%(rp[2]+1)
+        K2='C-%d'%(rp[1]+1)
+        K3='C-%d'%(rp[1]+1)
+        # K4
+        if rp[3]==0:
+            K4='C-NC'
+        elif rp[3]==1:
+            K4='C-NO'
+        else:
+            K4='invalid'
+        # K5 K9    
+        if rp[1]==0:
+            K5=K9='X'
+        elif rp[1]==1:
+            K5=K9='C-NC'
+        elif rp[1]==2:
+            K5=K9='C-NO'
+        else:
+            K5=K9='invalid'
+        # K6
+        if rp[6]==0:
+            K6='C-NC'
+        elif rp[6]==1:
+            K6='C-NO'
+        else:
+            K6='invalid'
+        # K7
+        if rp[4]==0:
+            K7='1-2, 3-4'
+        elif rp[4]==1:
+            K7='1-4, 2-3'
+        else:
+            K7='invalid'
+        # K8
+        if rp[5]==0:
+            K8='1-2, 3-4'
+        elif rp[5]==1:
+            K8='1-4, 2-3'
+        else:
+            K8='invalid'
+        for i in range(1,10):
+            s=eval('K%d'%i)
+            print "K%d: %s"%(i, s)
+        return ans
+    
     def on_sgSwitch_Direct_toggled(self, state):
         if state:
             print "Switch 1: direct"
-            ans=self.sw.ask('R1P0'*2)
+            ans=self.ask('R1P0')
     def on_sgSwitch_LF_toggled(self, state):
         if state:
             print "Switch 1: LF"
-            ans=self.sw.ask('R1P1'*2)
+            ans=self.ask('R1P1')
     def on_sgSwitch_HF_toggled(self, state):
         if state:
             print "Switch 1: HF"
-            ans=self.sw.ask('R1P2'*2)
+            ans=self.ask('R1P2')
     def on_TxSwitch_LF_toggled(self, state):
         if state:
             print "Switch 2: LF"
-            ans=self.sw.ask('R2P1'*2)
+            ans=self.ask('R2P1')
     def on_TxSwitch_HF_toggled(self, state):
         if state:
             print "Switch 2: HF"
-            ans=self.sw.ask('R2P2'*2)
+            ans=self.ask('R2P2')
     def on_TxSwitch_Term_toggled(self, state):
         if state:
             print "Switch 2: Term"
-            ans=self.sw.ask('R2P0'*2)
+            ans=self.ask('R2P0')
     def on_RxSwitch_LF_toggled(self, state):
         self.RxLF=state
         if state:
@@ -100,11 +152,11 @@ class SWController(QtGui.QMainWindow):
                     cmd='R3P0R4P0R5P0R6P1'                
                 else:
                     cmd='R3P0R4P1R5P0R6P1'                        
-        self.sw.ask(cmd*2)
+        self.ask(cmd)
         
         
 def main():
-    sw=visa.instrument('GPIB::2')
+    sw=visa.instrument('GPIB::2', term_chars = visa.LF)
     
     app = QtGui.QApplication(sys.argv)
     window = SWController(sw)
