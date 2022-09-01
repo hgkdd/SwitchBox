ec2fc3a10d3eee3eb97e60b9475594a554b33e7a
hgkdd <devnull@localhost>
2022-09-01 14:18:27 +0200

    refactor and small error corrections in RCSwitch_main.py

--- RCSwitch_main.py
+++ RCSwitch_main.py
@@ -14,9 +14,71 @@ class SWController(QtWidgets.QMainWindow):
     def __init__(self, sw):
         super(SWController, self).__init__()
         self.sw=sw
-        #print sw
-        #save settings
-        self.query('R1P0R2P0R3P1R4P1R5P0R6P0')
+        # 00: sig -> 40 dB -> term     usefull; safe
+        # 01: sig -> TX LF             useful; direkt LF
+        # 02: sig -> TX HF             usefull; direkt HF
+        # 10: sig -> LF amp -> term; LF PMs    usefull; LF AMP-Test
+        # 11: sig -> LF amp -> LxAnt; LF PMs    usefull; LF operation
+        # 12: sig -> LF amp -> HfAnt, LF PMs    usefull, LFAmp,HFAnt operation
+        # 20: sig -> HF amp -> term; HF PMs    usefull; HF AMP-Test
+        # 21: sig -> HF amp -> LfAnt, HF PMs    usefull, HFAmp,LFAnt operation
+        # 22: sig -> HF amp -> HfAnt, HF PMs   usefull, HF operation
+        self._states12 = {'LFAMP':
+                              {'LFANT': 'R1P1R2P1',
+                               'HFANT': 'R1P1R2P2',
+                               'TERM': 'R1P1R2P0'
+                               },
+                          'HFAMP':
+                              {'LFANT': 'R1P2R2P1',
+                                'HFANT': 'R1P2R2P2',
+                               'TERM': 'R1P2R2P0'
+                               },
+                          'DIRECT':
+                              {'LFANT': 'R1P0R2P1',
+                               'HFANT': 'R1P0R2P2',
+                               'TERM': 'R1P0R2P0'
+                               },
+                          'safe': 'R1P0R2P0'
+                          }
+        # LF:
+        #     0dB:
+        #            PM:  1010
+        #            REC: 1110
+        #    20dB:
+        #            PM:  1100
+        #            REC: 1000
+        # HF:
+        #     0dB:
+        #            PM:  0001
+        #            REC: 0101
+        #    20dB:
+        #            PM:  0111
+        #            REC: 0011
+        self._states3456 = {'LF': {'0dB': {'PM':    'R3P1R4P0R5P1R6P0',
+                                           'REC':   'R3P1R4P1R5P1R6P0',
+                                           'safe':  'R3P0R4P0R5P0R6P0'},
+                                   '20dB': {'PM':   'R3P1R4P1R5P0R6P0',
+                                            'REC':  'R3P1R4P0R5P0R6P0',
+                                            'safe': 'R3P0R4P0R5P0R6P0'}
+                                   },
+                            'HF': {'0dB': {'PM':    'R3P0R4P0R5P0R6P1',
+                                           'REC':   'R3P0R4P1R5P0R6P1',
+                                           'safe':  'R3P0R4P0R5P0R6P0'},
+                                   '20dB': {'PM':   'R3P0R4P1R5P1R6P1',
+                                            'REC':  'R3P0R4P0R5P1R6P1',
+                                            'safe': 'R3P0R4P0R5P0R6P0'}
+                                   },
+                            'safe': 'R3P0R4P0R5P0R6P0'
+                            }
+        # SG direkt in die Termination
+        # PM2 und REC an 50 Ohm (REC ueber 20 dB)
+        # Alle Antennen: open
+        # Fwd und Rev PM: undefiniert
+        self._safe = self._states12['safe'] + self._states3456['safe']      # 'R1P0R2P0R3P0R4P0R5P0R6P0'
+        # print sw
+        # save settings
+        self.query(self._safe)
+        self.query('R3P1R4P1')   # LFrx, Amp, PM
         self.RxLF=True
         self.RxAtt=True
         self.RxPM=True
@@ -33,61 +95,114 @@ class SWController(QtWidgets.QMainWindow):
 
     def doUpdate(self):
         # print 'Pling'
-        s=self.sw.query('')
-        self.monitor=True
+        s = self.sw.query('')
+        s3456 = s[11::4]
+        self.monitor = True
         #print s
-        rdict=dict(zip(map(int, s[1::4]), map(int, s[3::4]))) #keys: relais, value: position
-        s456=s[15::4]
-        
-        if rdict[1]==0: # direct
+        rdict = dict(zip(map(int, s[1::4]), map(int, s[3::4])))   # keys: relais, value: position
+        # 0000: PM2 -> 50 Ohm; REC -> 20 dB -> 50 Ohm          usefull, safe
+        # 1000: Pm2 -> 50 Ohm, Rec -> 20 dB -> Rx LF           usefull, LF -> 20dB -> REC
+        # 0100: PM2 -> 20 dB -> 50 Ohm, REC -> 50 Ohm          usefull, safe
+        # 1100: PM2 -> 20 dB -> Rx LF, REc -> 50 Ohm           usefull, LF -> 20 dB -> PM2
+        # 0010: PM2 -> 50 Ohm, REC -> 20 dB -> 50 Ohm  = 0000  usefull, safe
+        # 0110: PM2 -> 20 dB -> 50 Ohm, REC -> 50 Ohm  = 0100  usefull, safe
+        # 1010: PM2 -> Rx LF; REC -> 20 dB -> 50 Ohm           usefull, LF -> 0dB -> PM2
+        # 1110: PM2 -> 20 dB -> 50 Ohm, REC -> RX LF           usefull, LF -> 0dB -> REC
+        # 0001: PM2 -> Rx HF; REC -> 20 dB -> 50 Ohm           usefull, HF -> 0dB -> PM2
+        # 1001: Pm2 -> Rx HF, Rec -> 20 dB -> Rx LF            don't use; PM and REC both exposed
+        # 0101: PM2 -> 20 dB -> 50 Ohm, REC -> Rx HF           usefull; HF -> 0dB -> REC
+        # 1101: PM2 -> 20 dB -> rx LF, REC -> Rx HF            don't use; PM and REC both exposed
+        # 0011: PM2 -> 50 Ohm , REC -> 20 dB -> Rx HF          usefull; HF -> 20 dB -> REC
+        # 0111: PM2 -> 20 dB -> rx HF, REC -> 50 Ohm           usefull; HF -> 20 dB -> PM2
+        # 1011: PM2 -> Rx LF; REC -> 20 dB -> Rx HF            don't use, PM and REC both exposed
+        # 1111: PM2 -> 20 dB -> RX HF, REC -> RX LF            don't use, PM and REC both exposed
+        # LF:
+        #     0dB:
+        #            PM:  1010
+        #            REC: 1110
+        #    20dB:
+        #            PM:  1100
+        #            REC: 1000
+        # HF:
+        #     0dB:
+        #            PM:  0001
+        #            REC: 0101
+        #    20dB:
+        #            PM:  0111
+        #            REC: 0011
+        if rdict[1] == 0:  # direct
             self.ui.sgSwitch_Direct.click()
-        elif rdict[1]==1: # LF
+        elif rdict[1] == 1:  # LF
             self.ui.sgSwitch_LF.click()
-        elif rdict[1]==2: # HF
+        elif rdict[1] == 2:  # HF
             self.ui.sgSwitch_HF.click()
 
-        if rdict[2]==0: # TERM
+        if rdict[2] == 0:  # TERM
             self.ui.TxSwitch_Term.click()
-        elif rdict[2]==1: # LogPer
+        elif rdict[2] == 1:  # LogPer
             self.ui.TxSwitch_LF.click()
-        elif rdict[2]==2: # Horn
+        elif rdict[2] == 2:  # Horn
             self.ui.TxSwitch_HF.click()
 
-        if rdict[3]==0: # HF
+
+        # LF:
+        #     0dB:
+        #            PM:  1010
+        #            REC: 1110
+        #    20dB:
+        #            PM:  1100
+        #            REC: 1000
+        # HF:
+        #     0dB:
+        #            PM:  0001
+        #            REC: 0101
+        #    20dB:
+        #            PM:  0111
+        #            REC: 0011
+        if s3456 == '0111':
             self.ui.RxSwitch_HF.click()
-            if s456 == '111':
-                self.ui.RxSwitch_PM.click()
-                self.ui.AttSwitch_On.click()
-            elif s456 == '011':
-                self.ui.RxSwitch_Rec.click()
-                self.ui.AttSwitch_On.click()
-            elif s456 == '101':
-                self.ui.RxSwitch_PM.click()
-                self.ui.AttSwitch_Off.click()
-            elif s456 == '110':
-                self.ui.RxSwitch_Rec.click()
-                self.ui.AttSwitch_Off.click()
-            else:
-                print("'unvisible' switch: %r"%rdict)
-        elif rdict[3]==1: # LF
+            self.ui.RxSwitch_PM.click()
+            self.ui.AttSwitch_On.click()
+        elif s3456 == '0001':
+            self.ui.RxSwitch_HF.click()
+            self.ui.RxSwitch_PM.click()
+            self.ui.AttSwitch_Off.click()
+        elif s3456 == '0011':
+            self.ui.RxSwitch_HF.click()
+            self.ui.RxSwitch_Rec.click()
+            self.ui.AttSwitch_On.click()
+        elif s3456 == '0101':
+            self.ui.RxSwitch_HF.click()
+            self.ui.RxSwitch_Rec.click()
+            self.ui.AttSwitch_Off.click()
+        elif s3456 == '0000':
+            self.ui.RxSwitch_HF.click()
+            self.ui.RxSwitch_Rec.click()
+            self.ui.AttSwitch_On.click()
+        elif s3456 == '1100':
             self.ui.RxSwitch_LF.click()
-            if s456 == '100':
-                self.ui.RxSwitch_PM.click()
-                self.ui.AttSwitch_On.click()
-            elif s456 == '000':
-                self.ui.RxSwitch_Rec.click()
-                self.ui.AttSwitch_On.click()
-            elif s456 == '010':
-                self.ui.RxSwitch_PM.click()
-                self.ui.AttSwitch_Off.click()
-            elif s456 == '110':
-                self.ui.RxSwitch_Rec.click()
-                self.ui.AttSwitch_Off.click()
-            else:
-                print("'unvisible' switch: %r"%rdict)
+            self.ui.RxSwitch_PM.click()
+            self.ui.AttSwitch_On.click()
+        elif s3456 == '1010':
+            self.ui.RxSwitch_LF.click()
+            self.ui.RxSwitch_PM.click()
+            self.ui.AttSwitch_Off.click()
+        elif s3456 == '1000':
+            self.ui.RxSwitch_LF.click()
+            self.ui.RxSwitch_Rec.click()
+            self.ui.AttSwitch_On.click()
+        elif s3456 == '1110':
+            self.ui.RxSwitch_LF.click()
+            self.ui.RxSwitch_Rec.click()
+            self.ui.AttSwitch_Off.click()
+        elif s3456 == '0000':
+            self.ui.RxSwitch_LF.click()
+            self.ui.RxSwitch_Rec.click()
+            self.ui.AttSwitch_On.click()
+        else:
+            print("'invisible' switch: %r"%rdict)
         self.monitor=False
-        
-    
+
     def query(self, cmd):
         if not self.sw:
             return None
@@ -146,36 +261,44 @@ class SWController(QtWidgets.QMainWindow):
         if state:
             #print "Switch 1: direct"
             ans=self.query('R1P0')
+
     def on_sgSwitch_LF_toggled(self, state):
         if state:
             #print "Switch 1: LF"
             ans=self.query('R1P1')
+
     def on_sgSwitch_HF_toggled(self, state):
         if state:
             #print "Switch 1: HF"
             ans=self.query('R1P2')
+
     def on_TxSwitch_LF_toggled(self, state):
         if state:
             #print "Switch 2: LF"
             ans=self.query('R2P1')
+
     def on_TxSwitch_HF_toggled(self, state):
         if state:
             #print "Switch 2: HF"
             ans=self.query('R2P2')
+
     def on_TxSwitch_Term_toggled(self, state):
         if state:
             #print "Switch 2: Term"
             ans=self.query('R2P0')
+
     def on_RxSwitch_LF_toggled(self, state):
-        self.RxLF=state
+        self.RxLF = state
         # if state:
             #print "Switch 3: LF"
         self._rx_logic()
+
     def on_RxSwitch_HF_toggled(self, state):
-        self.RxLF=not(state)
+        self.RxLF = not(state)
         # if state:
             #print "Switch 3: HF"
         self._rx_logic()
+
     def on_AttSwitch_On_toggled(self, state):
         if not self.monitor:
             if (not state) and self.RxLF: # this may be dangerous
@@ -198,7 +321,7 @@ Do you really want to remove the attenuation?
                 if response != 'Yes':
                     self.ui.AttSwitch_On.toggle()
                     return
-        self.RxAtt=state
+        self.RxAtt = state
         if state:
             self.ui.conn_amp_direct_20.setEnabled(True)
             self.ui.conn_amp_direct_21.setEnabled(False)
@@ -214,43 +337,45 @@ Do you really want to remove the attenuation?
 #        if state:
 #            print "Switch 4: 0 dB"
 #        self._rx_logic()
+
     def on_RxSwitch_PM_toggled(self, state):
-        self.RxPM=state
+        self.RxPM = state
         #if state:
             #print "Switch 5: Rx PM"
         self._rx_logic()
+
     def on_RxSwitch_Rec_toggled(self, state):
-        self.RxPM=not(state)
+        self.RxPM = not(state)
         #if state:
             #print "Switch 5: Rx Rec"
         self._rx_logic()
         
     def _rx_logic(self):
-        lf=self.RxLF
-        att=self.RxAtt
-        pm=self.RxPM
+        lf = self.RxLF
+        att = self.RxAtt
+        pm = self.RxPM
         if lf:
             if att:
                 if pm:
-                    cmd='R3P1R4P1R5P0R6P0'
+                    cmd = self._states3456['LF']['20dB']['PM']   # 'R3P1R4P1R5P0R6P0'
                 else:
-                    cmd='R3P1R4P0R5P0R6P0'                
+                    cmd = self._states3456['LF']['20dB']['REC']   # 'R3P1R4P0R5P0R6P0'
             else:
                 if pm:
-                    cmd='R3P1R4P0R5P1R6P0'                
+                    cmd = self._states3456['LF']['0dB']['PM']   # 'R3P1R4P0R5P1R6P0'
                 else:
-                    cmd='R3P1R4P1R5P1R6P0'                
+                    cmd = self._states3456['LF']['0dB']['REC']   # 'R3P1R4P1R5P1R6P0'
         else:
             if att:
                 if pm:
-                    cmd='R3P0R4P1R5P1R6P1'
+                    cmd = self._states3456['HF']['20dB']['PM']   # 'R3P0R4P1R5P1R6P1'
                 else:
-                    cmd='R3P0R4P0R5P1R6P1'                
+                    cmd = self._states3456['HF']['20dB']['REC']   # 'R3P0R4P0R5P1R6P1'
             else:
                 if pm:
-                    cmd='R3P0R4P0R5P0R6P1'                
+                    cmd = self._states3456['HF']['0dB']['PM']   # 'R3P0R4P0R5P0R6P1'
                 else:
-                    cmd='R3P0R4P1R5P0R6P1'                        
+                    cmd = self._states3456['HF']['0dB']['REC']   # 'R3P0R4P1R5P0R6P1'
         if not self.monitor:
             self.query(cmd)
         
@@ -263,9 +388,9 @@ def main():
             sw.read_termination = '\n'
             sw.write_termination = '\n'
         except:
-            sw=None
+            sw = None
     else:
-        sw=None
+        sw = None
 
     app = QtWidgets.QApplication(sys.argv)
     window = SWController(sw)
